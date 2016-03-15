09c9a5c690fee2b170f076d6c77a1283704ea481
Amit Raturi <amit.raturi@tothenew.com>
2016-03-16 00:02:42 +0530

    1>quartz plugin added

--- grails-app/jobs/com/intelligrape/linksharing/SendEmailJob.groovy
+++ grails-app/jobs/com/intelligrape/linksharing/SendEmailJob.groovy
@@ -4,14 +4,15 @@ package com.intelligrape.linksharing
 
 class SendEmailJob {
     static triggers = {
-        cron name: 'myTrigger', cronExpression: " 0 55 22 15 3 ?"
+        simple name:'simpleTrigger', startDelay:10000, repeatInterval: 604800000, repeatCount: -1
+        cron name: 'myTrigger', cronExpression: " 0 0 13 ? ? ?"
     }
 //    def group = "MyGroup"
 //    def description = "Example job with Cron Trigger"
-//def useService
+def useService
     def execute() {
-//        EmailDTO emailDTO=new EmailDTO(body:"hello",subject: "my job",email:"amitraturi36@gmail.com" )
-//       useService.sendmail(emailDTO)
+        EmailDTO emailDTO=new EmailDTO(body:"hello",subject: "my job",email:"amitraturi36@gmail.com" )
+       useService.sendmail(emailDTO)
     }
 }
 
